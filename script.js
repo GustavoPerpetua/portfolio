84be4c1d87766ad686153d037f992377f8ac7b07
GustavoPerpetua <gustavoaperpe@gmail.com>
2023-11-25 03:39:35 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+let menuVisible = false;
+
+function showHideMenu(){
+    if (menuVisible){
+        document.getElementById("nav").classList="";
+        menuVisible = false
+    }else{
+        document.getElementById("nav").classList="responsive";
+        menuVisible = true
+    }
+}
+// to hide menu after click
+function select(){
+    document.getElementById("nav").classList ="";
+    menuVisible = false
+}
+
+function habilityEffect(){
+    let skills = document.getElementById("skills");
+    let skillsDistance = window.innerHeight - skills.getBoundingClientRect().top;
+    if(skillsDistance >= 300){
+        let habilities = document.getElementsByClassName("progress");
+        habilities[0].classList.add("javascript");
+        habilities[1].classList.add("htmlcss");
+        habilities[2].classList.add("python");
+        habilities[3].classList.add("sql");
+        habilities[4].classList.add("java");
+        habilities[5].classList.add("excel");
+        habilities[6].classList.add("tableau");
+        habilities[7].classList.add("comunication");
+        habilities[8].classList.add("teamwork");
+        habilities[9].classList.add("creativity");
+        habilities[10].classList.add("dedication");
+        habilities[11].classList.add("motivation");
+        habilities[12].classList.add("organized");
+        habilities[13].classList.add("eager");
+    }
+}
+
+window.onscroll = function(){
+    habilityEffect();
+}
+// min 1:46
